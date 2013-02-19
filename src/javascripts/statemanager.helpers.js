ef005529822ab89a69ca6d3f36d01f7b962b04b6
Mark Roseboom <mark@crashlytics.com>
2013-02-18 21:00:57 -0500

    adding js src

---
+++ src/javascripts/statemanager.helpers.js
@@ -0,0 +1,49 @@
+(function() {
+
+  StateManager.regExpStateConversion = function(name) {
+    name = name.replace(/[-[\]{}()+?.,\\^$|#\s]/g, '\\$&').replace(/:\w+/g, '([^\/]+)').replace(/\*\w+/g, '(.*?)');
+    return new RegExp("^" + name + "$");
+  };
+
+  StateManager.addStateManager = function(target, options) {
+    var stateManager, states;
+    if (options == null) {
+      options = {};
+    }
+    if (!target) {
+      new Error('Target must be defined');
+    }
+    states = _.isFunction(target.states) ? target.states() : target.states;
+    states = StateManager.bindStates(target, states);
+    stateManager = new Backbone.StateManager.Manager(states, options);
+    _.extend(target, {
+      states: states,
+      stateManager: stateManager,
+      triggerState: function() {
+        return stateManager.triggerState.apply(stateManager, arguments);
+      },
+      getCurrentStateName: function() {
+        return stateManager.getCurrentStateName();
+      }
+    });
+    if (options.initialize !== false) {
+      return target.stateManager.initialize(options);
+    }
+  };
+
+  StateManager.bindStates = function(target, states) {
+    states = _.clone(states);
+    _.each(states, function(state, pattern) {
+      _.each(['enter', 'exit'], function(key) {
+        if (state[key]) {
+          return state[key] = _.bind(state[key], target);
+        }
+      });
+      return _.each(state.transitions(function(method, pattern) {
+        return state[pattern] = _.bind(method, target);
+      }));
+    });
+    return states;
+  };
+
+}).call(this);
---
+++ src/javascripts/statemanager.transitions.js
@@ -0,0 +1,40 @@
+(function() {
+  var __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  StateManager.Transitions = (function(_super) {
+
+    __extends(Transitions, _super);
+
+    function Transitions() {
+      return Transitions.__super__.constructor.apply(this, arguments);
+    }
+
+    Transitions.prototype.model = StateManager.Transition;
+
+    Transitions.prototype.matchTransitions = function(type, stateName) {
+      return this.filter(function(transition) {
+        return transition.get('type') === type && transition.get('regExp').test(stateName) !== transition.get('inverse');
+      });
+    };
+
+    Transitions.prototype.parse = function(attrs) {
+      var key, transitions, value;
+      transitions = [];
+      if (_.isObject(attrs)) {
+        for (key in attrs) {
+          value = attrs[key];
+          transitions.push({
+            pattern: key,
+            method: value
+          });
+        }
+      }
+      return transitions;
+    };
+
+    return Transitions;
+
+  })(Backbone.Collection);
+
+}).call(this);
